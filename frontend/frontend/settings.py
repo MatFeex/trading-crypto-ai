231b94d75bbe43b4d2b8b479167de0b7e6bdbaa2
mfresson <mathieu.fresson@epfedu.fr>
2023-03-07 21:58:13 +0100

    allowed host *

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&t#lg4ztzd9@hhzu)gj(oqc+0xuvz!$#d^r!4tvj0ncz&hop56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 AUTH_USER_MODEL = 'bot.User'
 
